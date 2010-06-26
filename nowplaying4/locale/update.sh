6c632047b8ea1e354a9a444defe7414e301123cf
Szymon Tomasz Stefanek <pragma@kvirc.net>
2010-06-26 03:57:01 +0000

    Ported the nowplaying addon to the new installation system

---
+++ nowplaying4/locale/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+SOURCEFILES=../*.kvs
+
+POTFILE=nowplaying.pot
+
+xgettext -o $POTFILE -ktr $SOURCEFILES
+
+old=_old
+
+for lang in *.po; do
+	echo "Updating file $lang";
+	mv -f $lang $lang$old
+	msgmerge -o $lang $lang$old $POTFILE
+	if test -f $lang; then
+		rm -f $lang$old
+	fi
+done
+
